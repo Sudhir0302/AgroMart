de3596feffe4501b68d308e36cbf4762efe92f2c
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-01-19 20:17:06 +0530

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex items-center justify-center'>
+        <h1 className='bg-gray-600 text-white text-3xl w-fit'>Hello world!!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
