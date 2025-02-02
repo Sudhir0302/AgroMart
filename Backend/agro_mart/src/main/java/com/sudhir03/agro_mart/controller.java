89e999d5cafd33eddf329f4e4c4868606c540f52
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-02-02 23:21:41 +0530

    tested with frontend

--- Backend/agro_mart/src/main/java/com/sudhir03/agro_mart/controller.java
+++ Backend/agro_mart/src/main/java/com/sudhir03/agro_mart/controller.java
@@ -1,15 +1,13 @@
 package com.sudhir03.agro_mart;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class controller {
 
-    @CrossOrigin(origins = "*")
     @GetMapping("/home")
-    public String call(){
-        return "helloooo";
+    public void call(){
+        System.out.print("adf");
     }
 }
