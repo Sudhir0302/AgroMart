72d73ca013e2eaa61c8c7118df68ee49aaecab2d
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-02-02 22:52:08 +0530

    designed basic home structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Home from './Components/Home'
 import { Routes, Route, useLocation, Router } from 'react-router-dom';
+import Explore from './Components/Explore';
 
 function App() {
   return (
       <div>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/explore' element={<Explore />}/>
         </Routes>
       </div>
   )
--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React, { useRef } from 'react';
+import Navbar from './Navbar';
+import Products from './Products';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; 
+import DailySpecial from '../Categories/DailySpecial';
 
 const Home = () => {
-  return (
-    <div>
-        <Navbar />
-        <h1 className='text-center text-3xl'>AgroMart</h1>
-    </div>
-  )
-}
 
-export default Home
+  return (
+    <div className=''>
+      <Navbar />
+      <div className='flex flex-col'>
+        <h1 className='text-center text-3xl'>AgroMart</h1>
+        <div className='m-3'>
+          <DailySpecial />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
