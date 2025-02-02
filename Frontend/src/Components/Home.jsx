94d474a5489fd1a9d7765d08dee967300845ca39
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-02-02 23:23:59 +0530

    tested with backend

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,11 +1,19 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Navbar from './Navbar';
 import Products from './Products';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; 
 import DailySpecial from '../Categories/DailySpecial';
+import axios from 'axios';
 
 const Home = () => {
 
+  useEffect(()=>{
+      const fetch=async()=>{
+         const res=await axios.get("http://localhost:8080/home");
+         console.log(res);
+      }
+      fetch();
+  })
   return (
     <div className=''>
       <Navbar />
