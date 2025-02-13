62ce803b482e9ca380db310f9694838bb4e50c59
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-02-13 15:40:40 +0530

    created user model

--- Backend/agro_mart/src/main/java/com/sudhir03/agro_mart/controller/Usercontroller.java
+++ Backend/agro_mart/src/main/java/com/sudhir03/agro_mart/controller/Usercontroller.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-public class controller {
+public class Usercontroller {
 
     @Autowired
     private UserRepo userRepo;
--- Backend/agro_mart/src/main/java/com/sudhir03/agro_mart/model/User.java
+++ Backend/agro_mart/src/main/java/com/sudhir03/agro_mart/model/User.java
@@ -1,2 +1,72 @@
-package com.sudhir03.agro_mart.model;public class user {
+package com.sudhir03.agro_mart.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    public User() {}
+
+    public User(int id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
