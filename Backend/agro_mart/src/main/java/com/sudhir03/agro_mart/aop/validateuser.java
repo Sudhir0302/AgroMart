f98db9825361323e7018d671cf6e170a80778f98
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-03-31 00:29:19 +0530

    aop with  springsecurity

--- Backend/agro_mart/src/main/java/com/sudhir03/agro_mart/aop/validateuser.java
+++ Backend/agro_mart/src/main/java/com/sudhir03/agro_mart/aop/validateuser.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +18,11 @@ public class validateuser {
     @Around("execution(* com.sudhir03.agro_mart.controller.UserController.login(..))&&args(user)")
     public Object checkemail(ProceedingJoinPoint jp, UserDTO user) throws Throwable {
 
+        //security filter runs before aspect,hence it returns the error before coming to this jointpoint
+//        if(user.getUsername()==null||user.getPassword()==null){
+//            LOGGER.info("User details empty");
+//            return ResponseEntity.badRequest().body("Invalid email!!");
+//        }
         Object obj= jp.proceed(new Object[]{user});
         LOGGER.info("user validated: "+obj);
 
